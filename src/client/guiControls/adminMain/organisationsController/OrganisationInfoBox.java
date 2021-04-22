728182c0e7e49a2d26a9a4de8a2eb19913b2260a
autumnssuns <autumnssuns@gmail.com>
2021-04-23 01:14:39 +1000

    Update Client GUI (Admin) - No database: Assets and Organisations
    - Implement display in Assets and Organisations, allowing the addition of new assets and new organisations (as well as their assets).
    - Know issue: An organisation's stock is not saved correctly (would disappear on revisit)

---
+++ src/client/guiControls/adminMain/organisationsController/OrganisationInfoBox.java
@@ -0,0 +1,208 @@
+package client.guiControls.adminMain.organisationsController;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+/**
+ * A box to display user information and can be interacted with.
+ */
+public class OrganisationInfoBox extends HBox {
+    private int organisationId;
+    private String name;
+    private float credit;
+    private int assetQuantity;
+
+    private Label idLabel;
+    private Label nameLabel;
+    private Label creditLabel;
+    private Label assetQuantityLabel;
+    private VBox organisationalAssetsBox = new VBox();
+
+    private Button editButton;
+    private Button removeButton;
+
+    private OrganisationsController controller;
+
+    /**
+     * Initiates the box with asset information.
+     * @param organisationId The asset's id.
+     * @param name The name of the asset.
+     * @param credit The description of the asset.
+     */
+    public OrganisationInfoBox(int organisationId, String name, float credit, int assetQuantity){
+        super();
+        this.setAlignment(Pos.CENTER);
+        this.setPrefHeight(80);
+        this.setPrefWidth(1363);
+        this.setLayoutX(41);
+        this.setLayoutY(260);
+        this.setSpacing(20);
+
+        this.organisationId = organisationId;
+        this.name = name;
+        this.credit = credit;
+        this.assetQuantity = assetQuantity;
+
+        initiateNodes();
+
+        this.getChildren().addAll(idLabel, nameLabel, creditLabel, assetQuantityLabel, editButton, removeButton);
+    }
+
+    /**
+     * Draw the nodes displaying the asset's info.
+     */
+    private void initiateNodes(){
+        createIdLabel();
+        createNameLabel();
+        createCreditLabel();
+        createAssetQuantityLabel();
+        createEditButton();
+        createRemoveButton();
+    }
+
+    /**
+     * Creates a label to display the asset's id.
+     */
+    private void createIdLabel(){
+        idLabel = new Label(String.valueOf(organisationId));
+        idLabel.getStyleClass().add("blackLabel");
+        idLabel.setAlignment(Pos.CENTER);
+        idLabel.setPrefWidth(100);
+        idLabel.setPrefHeight(80);
+    }
+
+    /**
+     * Creates a text field to display the asset's name.
+     */
+    private void createNameLabel(){
+        nameLabel = new Label(name);
+        nameLabel.getStyleClass().add("blackLabel");
+        nameLabel.setPrefWidth(300);
+        nameLabel.setPrefHeight(30);
+    }
+
+    /**
+     * Reload the box using the stored asset's info.
+     */
+    public void reloadEntries(){
+        nameLabel.setText(name);
+        creditLabel.setText(String.valueOf(credit));
+    }
+
+    /**
+     * Creates a text field to display the asset's description.
+     */
+    private void createCreditLabel(){
+        creditLabel = new Label(String.valueOf(credit));
+        creditLabel.getStyleClass().add("blackLabel");
+        creditLabel.setPrefWidth(300);
+        creditLabel.setPrefHeight(30);
+    }
+
+    /**
+     * Creates a text field to display the asset's description.
+     */
+    private void createAssetQuantityLabel(){
+        assetQuantityLabel = new Label(String.valueOf(assetQuantity));
+        assetQuantityLabel.getStyleClass().add("blackLabel");
+        assetQuantityLabel.setPrefWidth(400);
+        assetQuantityLabel.setPrefHeight(30);
+    }
+
+    /**
+     * Creates a button that allows the admin to edit a asset's info.
+     */
+    private void createEditButton(){
+        editButton = new Button("Edit");
+        editButton.setPrefWidth(100);
+        editButton.setPrefHeight(30);
+        editButton.setOnAction(e -> startEdit());
+    }
+
+    /**
+     * Creates a button that allows the admin to remove an asset.
+     */
+    private void createRemoveButton(){
+        removeButton = new Button("Remove");
+        removeButton.setPrefWidth(100);
+        removeButton.setPrefHeight(30);
+        removeButton.setOnAction(e -> removeEntry());
+    }
+
+    /**
+     * Begins editing the current entry.
+     */
+    private void startEdit(){
+        controller.startEditor(this);
+    }
+
+    /**
+     * Set the controller for this box.
+     * @param controller The controller for this box.
+     */
+    public void setController(OrganisationsController controller){
+        this.controller = controller;
+    }
+
+    /**
+     * Returns the name of the organisation.
+     * @return The name of the organisation.
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * Returns the credit of the organisation.
+     * @return The credit of the organisation.
+     */
+    public float getCredit(){
+        return credit;
+    }
+
+    /**
+     * Returns the Assets Box of the current organisation.
+     * @return The Assets Box of the current organisation.
+     */
+    public VBox getOrganisationalAssetsBox(){
+        System.out.println(organisationalAssetsBox);
+        System.out.println(organisationalAssetsBox.getChildren().size());
+        return this.organisationalAssetsBox;
+    }
+
+    /**
+     * Sets a new assets box for the organisation.
+     * @param newBox The new assets box.
+     */
+    public void setOrganisationalAssetsBox(VBox newBox){
+        organisationalAssetsBox.getChildren().addAll(newBox.getChildren());
+        System.out.println(organisationalAssetsBox);
+        System.out.println(organisationalAssetsBox.getChildren().size());
+    }
+
+    /**
+     * Removes the current entry.
+     */
+    private void removeEntry() {
+        ((VBox) this.getParent()).getChildren().remove(this);
+    }
+
+    /**
+     * Sets the name of the current organisation to a new value.
+     * @param name The new name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets the credit of the current organisation to a new value.
+     * @param credit The new credit.
+     */
+    public void setCredit(float credit){
+        this.credit=credit;
+    }
+}
---
+++ src/client/guiControls/adminMain/organisationsController/OrganisationalAssetInfoBox.java
@@ -0,0 +1,168 @@
+package client.guiControls.adminMain.organisationsController;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+/**
+ * A box to display user information and can be interacted with.
+ */
+public class OrganisationalAssetInfoBox extends HBox {
+    private String name;
+    private int quantity;
+
+    private TextField nameTextField;
+    private TextField quantityTextField;
+
+    private Button editButton;
+    private Button removeButton;
+
+    /**
+     * Initiates the box with asset information.
+     * @param name The name of the asset.
+     * @param quantity The description of the asset.
+     */
+    public OrganisationalAssetInfoBox(String name, int quantity){
+        super();
+        this.setAlignment(Pos.CENTER);
+        this.setPrefHeight(80);
+        this.setPrefWidth(1363);
+        this.setLayoutX(41);
+        this.setLayoutY(260);
+        this.setSpacing(20);
+
+        this.name = name;
+        this.quantity = quantity;
+
+        initiateNodes();
+
+        this.getChildren().addAll(nameTextField, quantityTextField, editButton, removeButton);
+        disable();
+    }
+
+    /**
+     * Draw the nodes displaying the asset's info.
+     */
+    private void initiateNodes(){
+        createNameTextField();
+        createQuantityTextField();
+        createEditButton();
+        createRemoveButton();
+    }
+
+    /**
+     * Update the asset's info by taking data from the text fields.
+     */
+    private void updateValues(){
+        name = nameTextField.getText();
+        quantity = Integer.parseInt(quantityTextField.getText());
+    }
+
+    /**
+     * Reload the box using the stored asset's info.
+     */
+    private void reloadEntries(){
+        nameTextField.setText(name);
+        quantityTextField.setText(String.valueOf(quantity));
+    }
+
+    /**
+     * Creates a text field to display the asset's name.
+     */
+    private void createNameTextField(){
+        nameTextField = new TextField(name);
+        nameTextField.setPrefWidth(300);
+        nameTextField.setPrefHeight(30);
+    }
+
+    /**
+     * Creates a text field to display the asset's description.
+     */
+    private void createQuantityTextField(){
+        quantityTextField = new TextField(String.valueOf(quantity));
+        quantityTextField.setPrefWidth(700);
+        quantityTextField.setPrefHeight(30);
+    }
+
+    /**
+     * Creates a button that allows the admin to edit a asset's info.
+     */
+    private void createEditButton(){
+        editButton = new Button("Edit");
+        editButton.setPrefWidth(100);
+        editButton.setPrefHeight(30);
+        editButton.setOnAction(e -> startEdit());
+    }
+
+    /**
+     * Creates a button that allows the admin to remove an asset.
+     */
+    private void createRemoveButton(){
+        removeButton = new Button("Remove");
+        removeButton.setPrefWidth(100);
+        removeButton.setPrefHeight(30);
+        removeButton.setOnAction(e -> removeEntry());
+    }
+
+    /**
+     * Disables the current entry from being edited.
+     */
+    private void disable(){
+        nameTextField.setDisable(true);
+        quantityTextField.setDisable(true);
+    }
+
+    /**
+     * Enables the current entry to be edited.
+     */
+    private void enable(){
+        nameTextField.setDisable(false);
+        quantityTextField.setDisable(false);
+        editButton.setText("Confirm");
+        editButton.setOnAction(e -> startEdit());
+    }
+
+    /**
+     * Begins editing the current entry.
+     */
+    private void startEdit(){
+        enable();
+        editButton.setText("Confirm");
+        editButton.setOnAction(e -> confirmEdit());
+        removeButton.setText("Cancel");
+        removeButton.setOnAction(e -> cancelEdit());
+    }
+
+    /**
+     * Confirms the changes to the current entry.
+     */
+    private void confirmEdit() {
+        disable();
+        updateValues();
+        editButton.setText("Edit");
+        editButton.setOnAction(e -> startEdit());
+        removeButton.setText("Remove");
+        removeButton.setOnAction(e -> removeEntry());
+    }
+
+    /**
+     * Cancels the changes to the current entry.
+     */
+    private void cancelEdit(){
+        disable();
+        reloadEntries();
+        editButton.setText("Edit");
+        editButton.setOnAction(e -> startEdit());
+        removeButton.setText("Remove");
+        removeButton.setOnAction(e -> removeEntry());
+    }
+
+    /**
+     * Removes the current entry.
+     */
+    private void removeEntry() {
+        ((VBox) this.getParent()).getChildren().remove(this);
+    }
+}
