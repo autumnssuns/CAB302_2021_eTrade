fc04ece3a17addda187744b3c0be1c97c3371724
Daniel Pham <pduy091885@gmail.com>
2021-05-04 12:34:15 +1000

    Controllers Tests

---
+++ src/client/guiControls/adminMain/assetsController/AssetsControllerTest.java
@@ -0,0 +1,4 @@
+package client.guiControls.adminMain.assetsController;
+
+public class AssetsControllerTest {
+}
---
+++ src/client/guiControls/adminMain/usersController/UsersControllerTest.java
@@ -0,0 +1,4 @@
+package client.guiControls.adminMain.usersController;
+
+public class UsersControllerTest {
+}
