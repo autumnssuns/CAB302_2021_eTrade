ab5449eb88162937ad39348724928f8b5c22c39f
Dan Tran <67458114+autumnssuns@users.noreply.github.com>
2021-05-15 01:19:38 +1000

    Updates Request Test

--- src/common/RequestTest.java
+++ src/common/RequestTest.java
@@ -15,27 +15,27 @@ class RequestTest {
 
     @BeforeEach
     void setUp(){
-        String username = "username";
-        String password = "password";
+        String username = "dan";
+        String password = "123";
         User tempUser = new User(username, password);
         loginRequest = new Request(tempUser, "login");
 
         User user = new User(0, username, username, password, "user", 0);
         queryRequest = new Request(user, "query stock");
 
-        User userAsAttachment = new User(1, "Do ", "something", "with this", "user", 0);
+        User userAsAttachment = new User(1, "Dan Tran ", "dan", "123", "user", 0);
         updateRequest = new Request(user, "update", userAsAttachment);
+        updateRequest.setAttachmentType(User.class);
 
-        User userAsAttachment2 = new User(2, "Delete","Delete", "this", "user", 0);
+        User userAsAttachment2 = new User(2, "Rodo Nguyen","rodo", "rodo", "user", 0);
         deleteRequest = new Request(user, "delete", userAsAttachment2);
+        deleteRequest.setAttachmentType(User.class);
     }
 
     @Test
     void getSender() {
-        assertNull(loginRequest.getUser());
-        assertEquals("username", queryRequest.getUser());
-        assertEquals("username", updateRequest.getUser());
-        assertEquals("username", deleteRequest.getUser());
+        assertEquals(new User("dan", "123"), loginRequest.getUser());
+        assertEquals(new User(0, "dan", "dan", "123", "user", 0), queryRequest.getUser());
     }
 
     @Test
@@ -48,9 +48,17 @@ class RequestTest {
 
     @Test
     void getAttachment() {
-        assertEquals(new User("username", "password"), loginRequest.getAttachment());
+        assertNull(loginRequest.getAttachment());
         assertNull(queryRequest.getAttachment());
-        assertEquals(new User(1, "Do ", "something", "with this", "user", 0), updateRequest.getAttachment());
-        assertEquals(new User(2, "Delete","Delete", "this", "user", 0), deleteRequest.getAttachment());
+        assertEquals(new User(1, "Dan Tran ", "dan", "123", "user", 0), updateRequest.getAttachment());
+        assertEquals(new User(2, "Rodo Nguyen","rodo", "rodo", "user", 0), deleteRequest.getAttachment());
+    }
+
+    @Test
+    void getAttachmentType(){
+        assertNull(loginRequest.getAttachmentType());
+        assertNull(queryRequest.getAttachmentType());
+        assertEquals(User.class, updateRequest.getAttachmentType());
+        assertEquals(User.class, deleteRequest.getAttachmentType());
     }
 }
