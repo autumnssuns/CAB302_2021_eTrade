f21f1b921ca411560bcf4bb2a346327b618c17d4
Daniel Pham <pduy091885@gmail.com>
2021-05-22 12:29:59 +1000

    branch #37 materials

---
+++ src/main/java/server/Client.java
@@ -0,0 +1,70 @@
+package common;
+
+import common.dataClasses.User;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Provides functions for client to interact with the server from the GUI
+ */
+public class Client implements Serializable {
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private String address = "127.0.0.1";
+    private int port = 5678;
+
+
+
+    public void Start() throws IOException {
+        try {
+            socket = new Socket(address, port);
+            // Temporary prompt message
+            System.out.println("Connected");
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public  void Close() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Sends request to server using an ObjectOutputStream instance
+     * @param request The user's request
+     * @return the Response as an Object from server
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private Response sendRequest(Request request) throws IOException, ClassNotFoundException {
+        out.writeObject(request);
+        return (Response) in.readObject();
+    }
+    public static void main (String args[]) throws IOException, ClassNotFoundException {
+        Client client = new Client();
+        client.Start();
+        User testUser = new User("Duy", "123");
+        Request loginRequest = new Request(testUser,"Login" );
+        Response serverResponse = client.sendRequest(loginRequest);
+        User information = (User) serverResponse.getAttachment();
+        information.getAccountType();
+        information.getPassword();
+
+
+    }
+}
---
+++ src/main/java/server/DataSourceClasses/StockDataSource.java
@@ -0,0 +1,122 @@
+package server.DataSourceClasses;
+
+// return add , edit , query, delete , return all, get 1,
+// server, casetoresponse
+
+import common.dataClasses.DataCollection;
+import common.dataClasses.Stock;
+import server.DBconnection;
+
+import javax.xml.crypto.Data;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StockDataSource {
+    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `cab302_eTrade`.`stock` (\n" +
+                "   `organisation_id` INT NOT NULL,\n" +
+                "   `asset_id` INT NOT NULL,\n" +
+                "   `asset_quantity` INT NOT NULL DEFAULT 0,\n" +
+                " PRIMARY KEY (`organisation_id`, `asset_id`),\n" +
+                " CONSTRAINT `asset`\n" +
+                "   FOREIGN KEY (`asset_id`)\n" +
+                "   REFERENCES `cab302_eTrade`.`assets` (`asset_id`)\n" +
+                "   ON DELETE NO ACTION\n" +
+                "   ON UPDATE NO ACTION,\n" +
+                " CONSTRAINT `stock_organisation`\n" +
+                " FOREIGN KEY (`organisation_id`, `asset_id`)\n" +
+                "   REFERENCES `cab302_eTrade`.`organisations` (`organisation_id`)\n" +
+                "   ON DELETE NO ACTION\n" +
+                "   ON UPDATE NO ACTION)\n" +
+                " ENGINE = InnoDB;\n" +
+                "\n" +
+                " CREATE INDEX `asset_idx` ON `cab302_eTrade`.`stock` (`asset_id` ASC) VISIBLE;";
+    private static final String EDIT_QUANTITY=
+            "UPDATE stock" +
+            "   SET organisation_id=?, asset_id=?, asset_quantity=?" +
+            "   WHERE organisation=?, asset_id=? ";
+    private static final String GET_STOCK =
+            "SELECT * FROM stock WHERE organisation_id = ?";
+    private static final String INSERT_ASSET =
+            "INSERT INTO stock(organisation_id, asset_id, asset_quantity) " +
+            "VALUES (?,?,?)";
+
+    private Connection connection;
+    private PreparedStatement editQuantity;
+    private PreparedStatement insertAsset;
+    private PreparedStatement getStock;
+
+    /**
+     * Connect to the Stock database and create one if not exists
+     */
+    public StockDataSource() {
+        connection = DBconnection.getInstance();
+        try {
+            Statement st = connection.createStatement();
+            st.execute(CREATE_TABLE);
+            editQuantity = connection.prepareStatement(EDIT_QUANTITY);
+            getStock = connection.prepareStatement(GET_STOCK);
+            insertAsset = connection.prepareStatement(INSERT_ASSET);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Edit quantity of an asset of an organization
+     * @param organisationID ~
+     * @param assetID ~
+     * @param assetQuantity ~
+     */
+    public void editQuantity(int organisationID, int assetID, int assetQuantity) {
+        try {
+            editQuantity.setInt(1, organisationID);
+            editQuantity.setInt(2, assetID);
+            editQuantity.setInt(3, assetQuantity);
+            editQuantity.setInt(4, organisationID);
+            editQuantity.setInt(5, assetID);
+            editQuantity.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Insert new asset with its quantity to an organization
+     * @param organisationId ~
+     * @param assetId ~
+     * @param assetQuantity ~
+     */
+    public void insertAsset(int organisationId, int assetId, int assetQuantity) {
+        try {
+            insertAsset.setInt(1, organisationId);
+            insertAsset.setInt(2, assetId);
+            insertAsset.setInt(3, assetQuantity);
+            insertAsset.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get stocks list from an organization
+     * @param organisationId ~
+     * @return an Object array of {asset_id, organization_id, asset_quantity}
+     */
+    public Object[] getStock(int organisationId) {
+        Object[] stocks = new Object[]{};
+        ArrayList<Object> temp = new ArrayList<Object>(Arrays.asList(stocks));
+        try {
+            getStock.setInt(1, organisationId);
+            ResultSet rs = getStock.executeQuery();
+            while(rs.next()) {
+                temp.add(new int[]{rs.getInt("asset_id"), rs.getInt("organization_id"), rs.getInt("asset_quantity")});
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return temp.toArray();
+    }
+}
+
+
