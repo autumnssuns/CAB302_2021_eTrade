9fa8af3f100b141d211cc999c861c9502667bc3d
Rodo <rodonguyendd@gmail.com>
2021-05-25 14:32:08 +1000

    Added Unit Tests

---
+++ src/test/java/common/dataClasses/BalanceException.java
@@ -0,0 +1,19 @@
+package common.dataClasses;
+
+class BalanceException extends Exception {
+    public BalanceException() {
+        super();
+    }
+
+    public BalanceException(String message) {
+        super(message);
+    }
+
+    public BalanceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BalanceException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/test/java/common/dataClasses/StockTest.java
@@ -0,0 +1,31 @@
+package common.dataClasses;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StockTest {
+
+    Stock stock;
+
+    @Test
+    void setUnitId() {
+
+    }
+
+    @Test
+    void getUnitId() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
