fbfa8945ce47e6168ad710c02e6e8a995c4b757c
Rodo <rodonguyendd@gmail.com>
2021-05-26 16:31:42 +1000

    Fixed AssetsDataSource.java

--- src/main/java/server/Excluded_PUT_ALL_EXCLUSIONS_HERE/Features/TestDatabase.java
+++ src/main/java/server/Excluded_PUT_ALL_EXCLUSIONS_HERE/Features/TestDatabase.java
@@ -5,8 +5,5 @@ import server.Excluded_PUT_ALL_EXCLUSIONS_HERE.DataSourceClasses.UserDataSource;
 
 public class TestDatabase {
     public static void main(String[] args){
-        UserDataSource users = new UserDataSource();
-        User testuser = new User(0, "Rodo", "DUYANH", "123", "user", 1);
-        users.addUser(testuser);
     }
 }
