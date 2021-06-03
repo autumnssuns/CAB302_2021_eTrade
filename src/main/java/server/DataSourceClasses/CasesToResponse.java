5e36c2dbcb3c0111ad599902e6bcfcb1bc035d9b
Dan Tran <67458114+autumnssuns@users.noreply.github.com>
2021-06-03 14:30:32 +1000

    (Admin organisation) Added filter for items already in stock

--- src/main/java/server/DataSourceClasses/CasesToResponse.java
+++ src/main/java/server/DataSourceClasses/CasesToResponse.java
@@ -363,7 +363,7 @@ public class CasesToResponse  {
      */
     public static Response edit(Stock attachment){
         StockDataSource stockDataSource = new StockDataSource();
-        stockDataSource.editItemQuantity(attachment);
+        stockDataSource.updateUnitStock(attachment);
         Response response = new Response(true, attachment);
         return response;
     }
